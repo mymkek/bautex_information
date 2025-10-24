d178a41d697420dfded5978e3daca848fa9b185e
mymkek <mymkek@gmail.com>
2025-10-24 22:50:11 +0700

    configure project structure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // позволяет подключаться не только с localhost
+    port: 5173, // dev-порт, можно оставить
+    strictPort: true,
+    allowedHosts: ['information.bautex.pro', 'localhost'], // разрешённые хосты
+  },
 })
